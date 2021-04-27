253614f2d1155635e8611d27397ae951b57ceec1
Oleksandr Parfionov <alex@elinik.com>
2021-04-27 23:01:25 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,79 @@
+import asyncio
+import os
+
+from aioredis import create_redis_pool
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import uvicorn
+
+
+class Message(BaseModel):
+    value: str
+    ttl: int
+
+
+app = FastAPI(debug=False)
+
+
+@app.on_event('startup')
+async def startup_event():
+    redis_uri = os.environ.get('REDIS_URI', 'redis://127.0.0.1:6379/0?encoding=utf-8')
+    app.state.redis = await create_redis_pool(redis_uri)
+
+
+@app.on_event('shutdown')
+async def shutdown_event():
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
+    
+
+async def get_message_timeout(key: str):
+    msg = await app.state.redis.get(key)
+    if msg:
+        return msg
+        
+    channels = app.state.redis.channels
+    ch = channels[f'channel:{key}'.encode()]
+
+    while (await ch.wait_message()):
+        await ch.get()
+        await app.state.redis.unsubscribe(f'channel:{key}')
+
+    msg = await app.state.redis.get(key)
+
+    return msg
+
+
+@app.get('/messages/{key}')
+async def get_message(key: str, timeout:int = 0):
+    if timeout == 0:
+        msg = await app.state.redis.get(key)
+        if not msg:
+            raise HTTPException(status_code=404, detail="Message with given key not found.")
+    
+    await app.state.redis.subscribe(f'channel:{key}')
+    try:
+        msg = await asyncio.wait_for(get_message_timeout(key), timeout)
+    except asyncio.TimeoutError:
+        msg = None
+
+    if not msg:
+        raise HTTPException(status_code=404, detail="Message with given key not found.")
+
+    return {
+        "message": msg,
+    }
+
+
+@app.post('/messages/{key}')
+async def post_message(key: str, msg: Message):
+    res = await app.state.redis.execute('set', key, msg.value, 'nx', 'ex', msg.ttl)
+    
+    if not res:
+        raise HTTPException(status_code=409, detail="Message with given key already exist.")
+
+    await app.state.redis.publish(f'channel:{key}', 'channel message')
+
+
+if __name__ == '__main__':
+    uvicorn.run("api:app", host='0.0.0.0', port=8000, workers=4, reload=True)
---
+++ test_api.py
@@ -0,0 +1,123 @@
+import asyncio
+
+import pytest
+from asgi_lifespan import LifespanManager
+from httpx import AsyncClient
+
+from api import app
+
+
+@pytest.fixture()
+async def client():
+    async with AsyncClient(app=app, base_url="http://test") as client, LifespanManager(app):
+        await app.state.redis.execute('del', '1')
+        await app.state.redis.execute('del', '2')
+        await app.state.redis.execute('del', '3')
+        yield client
+
+
+@pytest.mark.asyncio
+async def test_post(client):
+    s = 'Some important message'
+    ttl = 10
+    timeout = 1
+
+    res1 = await client.post(
+        '/messages/1',
+        headers = {
+            'Content-Type': 'application/json'
+        },
+        json={
+            'value': s,
+            'ttl': ttl,
+        }
+    )
+
+    assert res1.status_code == 200
+
+    res2 = await client.post(
+        '/messages/1',
+        headers = {
+            'Content-Type': 'application/json'
+        },
+        json={
+            'value': s,
+            'ttl': ttl,
+        }
+    )
+
+    assert res2.status_code == 409
+
+
+@pytest.mark.asyncio
+async def test_get_after_post(client):
+    s = 'Some important message'
+    ttl = 20
+    timeout = 1
+
+    await client.post(
+        '/messages/2',
+        headers = {
+            'Content-Type': 'application/json'
+        },
+        json={
+            'value': s,
+            'ttl': ttl,
+        }
+    )
+
+    for i in range(10_000):
+        print(i)
+        res = await client.get('/messages/2', params={'timeout': timeout})
+        data = res.json()
+        assert res.status_code == 200
+        assert data['message'] == s
+
+
+
+@pytest.mark.asyncio
+async def test_get_before_post(client):
+    s = 'Some important message'
+    ttl = 30
+    timeout = 10
+    sleep_time = 5
+
+    async def helper_fn():
+        await asyncio.sleep(sleep_time)
+        await client.post(
+            '/messages/3',
+            headers = {
+                'Content-Type': 'application/json'
+            },
+            json={
+                'value': s,
+                'ttl': ttl,
+            }
+        )
+    
+    asyncio.create_task(helper_fn())
+
+    for i in range(100):
+        print(i)
+        res = await client.get('/messages/3', params={'timeout': timeout})
+        data = res.json()
+        assert res.status_code == 200
+        assert data['message'] == s
+
+
+@pytest.mark.asyncio
+async def test_get_without_post_timeout(client):
+    timeout = 5
+
+    res = await client.get('/messages/4', params={'timeout': timeout})
+    data = res.json()
+    assert res.status_code == 404
+    assert data['detail'] == 'Message with given key not found.'
+
+
+@pytest.mark.asyncio
+async def test_get_without_post_no_timeout(client):
+    res = await client.get('/messages/5')
+    data = res.json()
+    assert res.status_code == 404
+    assert data['detail'] == 'Message with given key not found.'
